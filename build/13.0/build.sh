132a72eabbcc23f587ce20a4ad97c5eed68eaeb6
ccdos <ccdos@intoerp.com>
2021-02-11 12:48:31 +0800

    add  odoo 13

--- build/13.0/build.sh
+++ build/13.0/build.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname "$0")/
 docker login -u odooeasy
-docker build -t odooeasy/intoodoo:10.0 .
-docker push  odooeasy/intoodoo:10.0
+docker build -t odooeasy/intoodoo:13.0 .
+docker push  odooeasy/intoodoo:13.0
